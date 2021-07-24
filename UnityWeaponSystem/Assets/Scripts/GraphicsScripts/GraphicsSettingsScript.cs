af0a5b6b409f0696456a8c2d79671425bda89a98
Daniel <danielnijkamp2004@gmail.com>
2021-07-25 01:31:38 +0200

    Added Graphics settings

---
+++ UnityWeaponSystem/Assets/Scripts/GraphicsScripts/GraphicsSettingsScript.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
+using TMPro;
+
+public class GraphicsSettingsScript : MonoBehaviour
+{
+    Resolution[] resolutions;
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
+    public PostProcessVolume m_Volume;
+
+    public Toggle m_toggle;
+
+    
+    void Start()
+    {
+
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        int currentResolutionIndex = 0;
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    public void SetFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+    public void EnablePostProcessing()
+    {
+        if (m_toggle.isOn == true)
+        {
+            m_Volume.enabled = true;
+        }
+        else
+        {
+            m_Volume.enabled = false;
+        }
+
+    }
+   
+}
+   
+
+    
